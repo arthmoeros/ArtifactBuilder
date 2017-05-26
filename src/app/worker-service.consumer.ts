7c3eb55827e70fa96e28005774b33d94781de195
Arturo Saavedra <artu.saavedra@gmail.com>
2017-05-26 00:39:16 -0400

    Some browsers gave issues with blob download, using trickier method instead

--- src/app/worker-service.consumer.ts
+++ src/app/worker-service.consumer.ts
@@ -15,6 +15,10 @@ export class WorkerServiceConsumer {
         artifactGenerationRequest: {
             devEndpoint: "http://localhost:8080/artifactGenerationRequest",
             endpoint: "https://artifacter-worker.herokuapp.com/artifactGenerationRequest"
+        },
+        generatedArtifacts: {
+            devEndpoint: "http://localhost:8080/generatedArtifacts",
+            endpoint: "https://artifacter-worker.herokuapp.com/generatedArtifacts"
         }
     }
 
@@ -37,7 +41,7 @@ export class WorkerServiceConsumer {
         jsonRequest.map = map;
 
         let headers = new Headers({ 'Content-Type': 'application/json' });
-        let options = new RequestOptions({ headers: headers, responseType: ResponseContentType.Blob });
+        let options = new RequestOptions({ headers: headers, responseType: ResponseContentType.Json });
         let endpoint = "";
         if(deployed != null){
             endpoint = WorkerServiceConsumer.config.artifactGenerationRequest.endpoint;
@@ -46,7 +50,26 @@ export class WorkerServiceConsumer {
         }
         let obs: Observable<Response> = this.http.post(endpoint, JSON.stringify(jsonRequest), options);
         obs.subscribe((response) => {
-            fileSaver(response.blob(), "result.zip");
+            let respUUID: string = response.json().artifactsUUID;
+            
+            let endpoint: string = "";
+            if(deployed != null){
+                endpoint = WorkerServiceConsumer.config.generatedArtifacts.endpoint;
+            }else{
+                endpoint = WorkerServiceConsumer.config.generatedArtifacts.devEndpoint;
+            }
+            endpoint = endpoint + "?uuid=" + respUUID;
+
+            if(document.getElementById("downloaderFrame") == null){
+                let iframe: HTMLIFrameElement = document.createElement("iframe");
+                iframe.name = "downloader";
+                iframe.id = "downloaderFrame";
+                iframe.style.display = "none";
+                document.body.appendChild(iframe);
+            }
+
+            window.open(endpoint, "downloader");
+            //fileSaver(response.blob(), "result.zip");
             document.getElementById("loadingBlocker").className = "app-blocker-loading hidden";
         }, (error) => {
             alert("An error ocurred while retrieving the Worker response: "+error);
