dd6ab5f53df4c3553c289dc453932823eae72757
Arturo Saavedra <artu.saavedra@gmail.com>
2017-07-27 21:51:30 -0400

    argh

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from "@angular/router";
+import { FormsModule }    from '@angular/forms';
+import { HttpModule } from "@angular/http";
+
+import { AppComponent } from './app.component';
+import { GeneratorIndexComponent } from './generator-index/generator-index.component';
+import { ArtifacterCoreService } from "./artifacter-core.service";
+import { FormRendererComponent } from './form-renderer/form-renderer.component';
+import { JsonIteratorPipe } from './json-iterator.pipe';
+import { InputRendererComponent } from './input-renderer/input-renderer.component';
+import { AppRoutingModule } from './app-routing/app-routing.module'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GeneratorIndexComponent,
+    FormRendererComponent,
+    JsonIteratorPipe,
+    InputRendererComponent,
+  ],
+  imports: [
+    HttpModule,
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [ArtifacterCoreService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
